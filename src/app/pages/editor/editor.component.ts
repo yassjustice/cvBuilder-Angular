48b496240641ff4e19b6d81d709ccfc6bd838c20
Yassir hakimi <yassirhakimi60@gmail.com>
2024-02-07 15:15:39 +0100

    editor update

--- src/app/pages/editor/editor.component.ts
+++ src/app/pages/editor/editor.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-editor',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
   templateUrl: './editor.component.html',
-  styleUrl: './editor.component.css'
+  styleUrl: './editor.component.css',
 })
 export class EditorComponent {
-
+  user: any = {
+    name: '',
+    email: '',
+    image: '',
+    desc: '',
+    skills: [],
+    experiences: [],
+    education: [],
+    address: '',
+  };
+  onSubmit() {
+    console.log(this.user);
+  }
 }
